2fe0db9d9f45afe342bd706d286a425f61421e2b
KevenJunco <49709096+KevenJunco@users.noreply.github.com>
2019-09-20 20:59:04 -0300

    Initial commit

---
+++ emerson/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+    const Header = () => (  <Header id="main-header">JSHunt</Header>
+
+
+    );
+    export default Header;
+
+    /*class  Header extends Component {
+        render (){
+
+            return <h1>Hello World</h1>
+        };
+*/
+    
